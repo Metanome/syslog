656af0f25da832e428e231c925dd7255128a2de0
John Manero <jmanero@dyn.com>
2013-05-08 14:28:35 -0400

    Use updated tail module

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var Tail = require('metanome-tail');
 
-var Syslog = module.exports = new Tail('/var/log/messages', "syslog.messages");
+var Syslog = module.exports = new Tail({
+	path : '/var/log/messages',
+	prefix : "syslog.messages",
+	root : "jmanero.foobar"
+});
 Syslog.interval = 5;
